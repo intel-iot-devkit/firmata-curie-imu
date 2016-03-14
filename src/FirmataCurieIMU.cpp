5fac763d4a0bf7bfbbc235f474204b9b243228d5
Justin Zemlyansky <jlstigman@live.com>
2016-03-13 17:31:59 -0700

    firmata curie imu wip
    work in progress

--- src/FirmataCurieIMU.cpp
+++ src/FirmataCurieIMU.cpp
@@ -1,5 +1,5 @@
 /*
-  FirmataCurieIMU.cpp
+FirmataCurieIMU.cpp
 */
 
 #include <ConfigurableFirmata.h>
@@ -13,7 +13,7 @@ FirmataCurieIMU::FirmataCurieIMU()
 
 boolean FirmataCurieIMU::handlePinMode(byte pin, int mode)
 {
-  return false;
+    return false;
 }
 
 void FirmataCurieIMU::handleCapability(byte pin)
@@ -22,55 +22,148 @@ void FirmataCurieIMU::handleCapability(byte pin)
 
 boolean FirmataCurieIMU::handleSysex(byte command, byte argc, byte *argv)
 {
-  if (command == CURIE_IMU)
-  {
-    byte imuCommand;
-    imuCommand = argv[0];
-
-    if (imuCommand == CURIE_IMU_READ_ACCEL)
+    if (command == CURIE_IMU)
     {
-      readAccelerometer();
-      return true;
-    }
+        byte imuCommand;
+        imuCommand = argv[0];
 
-    if (imuCommand == CURIE_IMU_READ_GYRO)
-    {
-      readGyro();
-      return true;
+        if (imuCommand == CURIE_IMU_READ_ACCEL)
+        {
+            readAccelerometer();
+            return true;
+        }
+
+        if (imuCommand == CURIE_IMU_READ_GYRO)
+        {
+            readGyro();
+            return true;
+        }
+        if (imuCommand == CURIE_IMU_READ_TEMP)
+        {
+            readTemp();
+            return true;
+        }
     }
-  }
-  return false;
+    return false;
 }
 
 void FirmataCurieIMU::reset()
 {
-  // initialize device
-  CurieIMU.begin();
+    // initialize device
+    CurieIMU.begin();
 
-  // Set the accelerometer range to 2G
-  CurieIMU.setAccelerometerRange(2);
+    // Set the accelerometer range to 2G
+    CurieIMU.setAccelerometerRange(2);
+
+    // set the gyro range 2000 (+/-2000°/s)
+    CurieIMU.setGyroRange(2000);
+
+    //read temperature in celcius 
+    CurieIMU.readTemp();
+
+    //shock detected enabled
+    CurieIMU.shockDetected();
+
+    //step counter enabled
+    CurieIMU.enableStepCounter();
+    
+
+
 }
 
 // FirmataCurieIMU interface functions
 
 void FirmataCurieIMU::readAccelerometer()
 {
-  int xAxis, yAxis, zAxis;
+    int xAxis, yAxis, zAxis;
 
-  CurieIMU.readAccelerometer(xAxis, yAxis, zAxis);
+    CurieIMU.readAccelerometer(xAxis, yAxis, zAxis);
 
-  Firmata.write(START_SYSEX);
-  Firmata.write(CURIE_IMU);
-  Firmata.write(CURIE_IMU_READ_ACCEL);
-  Firmata.write((byte)xAxis & 0x7F);
-  Firmata.write((byte)(xAxis >> 7) & 0x7F);
-  Firmata.write((byte)yAxis & 0x7F);
-  Firmata.write((byte)(yAxis >> 7) & 0x7F);
-  Firmata.write((byte)zAxis & 0x7F);
-  Firmata.write((byte)(zAxis >> 7) & 0x7F);
-  Firmata.write(END_SYSEX);
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_READ_ACCEL);
+    Firmata.write((byte)xAxis & 0x7F);
+    Firmata.write((byte)(xAxis >> 7) & 0x7F);
+    Firmata.write((byte)yAxis & 0x7F);
+    Firmata.write((byte)(yAxis >> 7) & 0x7F);
+    Firmata.write((byte)zAxis & 0x7F);
+    Firmata.write((byte)(zAxis >> 7) & 0x7F);
+    Firmata.write(END_SYSEX);
 }
 
 void FirmataCurieIMU::readGyro()
 {
+    int xAxis, yAxis, zAxis;
+
+    CurieIMU.readGyro(xAxis, yAxis, zAxis);
+
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_READ_GYRO);
+    Firmata.write((byte)xAxis & 0x7F);
+    Firmata.write((byte)(xAxis >> 7) & 0x7F);
+    Firmata.write((byte)yAxis & 0x7F);
+    Firmata.write((byte)(yAxis >> 7) & 0x7F);
+    Firmata.write((byte)zAxis & 0x7F);
+    Firmata.write((byte)(zAxis >> 7) & 0x7F);
+    Firmata.write(END_SYSEX);
+
+}
+void FirmataCurieIMU::readTemp()
+{
+    float Celcius;
+    CurieIMU.readTemp(Celcius);
+
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_READ_TEMP);
+    Firmata.write((byte)Celcius & 0x7F);
+    Firmata.write((byte)(Celcius >> 7) & 0x7F);
+    Firmata.write(END_SYSEX);
+}
+void FirmataCurieIMU::shockDetected() 
+{
+    
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_SHOCK_DETECT);
+    Firmata.write(END_SYSEX);
+}
+void FirmataCurieIMU::enableStepCounter()
+{
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_STEP_COUNTER);
+    Firmata.write(END_SYSEX);
+}
+void FirmataCurieIMU::enableTapDetection()
+{
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_TAP_DETECT);
+    Firmata.write(END_SYSEX);
+}
+void FirmataCurieIMU::readMotion();
+{
+
+    int axAxis, ayAxis, azAxis, gxAxis, gyAxis, gzAxis;
+
+    CurieIMU.readGyro(gxAxis, gyAxis, gzAxis);
+
+    Firmata.write(START_SYSEX);
+    Firmata.write(CURIE_IMU);
+    Firmata.write(CURIE_IMU_READ_MOTION);
+    Firmata.write((byte)gxAxis & 0x7F);
+    Firmata.write((byte)(gxAxis >> 7) & 0x7F);
+    Firmata.write((byte)gyAxis & 0x7F);
+    Firmata.write((byte)(gyAxis >> 7) & 0x7F);
+    Firmata.write((byte)gzAxis & 0x7F);
+    Firmata.write((byte)(gzAxis >> 7) & 0x7F);
+    Firmata.write((byte)axAxis & 0x7F);
+    Firmata.write((byte)(axAxis >> 7) & 0x7F);
+    Firmata.write((byte)ayAxis & 0x7F);
+    Firmata.write((byte)(ayAxis >> 7) & 0x7F);
+    Firmata.write((byte)azAxis & 0x7F);
+    Firmata.write((byte)(azAxis >> 7) & 0x7F);
+    Firmata.write(END_SYSEX);
 }
--- src/FirmataCurieIMU.h
+++ src/FirmataCurieIMU.h
@@ -1,5 +1,5 @@
 /*
-  FirmataCurieIMU.h
+FirmataCurieIMU.h
 */
 
 #ifndef FirmataCurieIMU_h
@@ -13,21 +13,31 @@
 
 #define CURIE_IMU_READ_ACCEL        (0x00)
 #define CURIE_IMU_READ_GYRO         (0x01)
+#define CURIE_IMU_READ_TEMP         (0x02)
+#define CURIE_IMU_SHOCK_DETECT      (0x03)
+#define CURIE_IMU_STEP_COUNTER      (0x04)
+#define CURIE_IMU_TAP_DETECT        (0x05)
+#define CURIE_IMU_READ_MOTION       (0x06)
 
-class FirmataCurieIMU:public FirmataFeature
+class FirmataCurieIMU :public FirmataFeature
 {
 public:
-  FirmataCurieIMU();
+    FirmataCurieIMU();
 
-  // FirmataFeature interface functions. Required for any Firmata plugin.
-  boolean handlePinMode(byte pin, int mode);
-  void handleCapability(byte pin);
-  boolean handleSysex(byte command, byte argc, byte *argv);
-  void reset();
+    // FirmataFeature interface functions. Required for any Firmata plugin.
+    boolean handlePinMode(byte pin, int mode);
+    void handleCapability(byte pin);
+    boolean handleSysex(byte command, byte argc, byte *argv);
+    void reset();
 
-  // FirmataCurieIMU interface functions
-  void readAccelerometer();
-  void readGyro();
+    // FirmataCurieIMU interface functions
+    void readAccelerometer();
+    void readGyro();
+    void readTemp();
+    void shockDetected();
+    void enableStepCounter();
+    void enableTapDetection();
+    void readMotion();
 };
 
 #endif
