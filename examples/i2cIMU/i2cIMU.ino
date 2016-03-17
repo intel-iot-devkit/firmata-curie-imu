7c6bb89ce27664757ccce52a2ac0e0eb4018d0e2
Justin Zemlyansky <jlstigman@live.com>
2016-03-17 13:08:48 -0700

    added simple i2c example

---
+++ examples/i2cIMU/i2cIMU.ino
@@ -0,0 +1,40 @@
+/*
+  Simple Firmata with CurieIMU + 12c
+*/
+
+#include <ConfigurableFirmata.h>
+#include <FirmataExt.h>
+FirmataExt firmataExt;
+
+#include <CurieIMU.h>
+#include <FirmataCurieIMU.h>
+FirmataCurieIMU curieIMU;
+
+#include <I2CFirmata.h>
+I2CFirmata myI2C;
+
+void systemResetCallback()
+{
+  firmataExt.reset();
+}
+
+void setup()
+{
+  Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
+
+  firmataExt.addFeature(curieIMU);
+  firmataExt.addFeature(myI2C);
+
+  Firmata.attach(SYSTEM_RESET, systemResetCallback);
+  Firmata.begin(57600);
+
+  // set reset to default config
+  systemResetCallback();
+}
+
+void loop()
+{
+  while (Firmata.available()) {
+    Firmata.processInput();
+  }
+}
